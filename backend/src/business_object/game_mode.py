76079020e943aedb9768365bee4ab642aa4be756
audelyra20-boop <audelyra20@gmail.com>
2026-09-04 09:16:45 +0000

    m

---
+++ backend/src/business_object/game_mode.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from business_object_object.game import Game
+from business_object_object.player import Player
+
+
+class GameMode(ABC):
+
+    """Abstract class that defines generic game mode."""
+
+    @abstractmethod
+    def play(self, player1: Player, player2: Player) -> Game:
+        pass
